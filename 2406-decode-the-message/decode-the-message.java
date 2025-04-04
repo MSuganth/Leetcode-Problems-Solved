06858461cca2c30fbc2e26a295ebd2dba0c65fbb
MSuganth <146002880+MSuganth@users.noreply.github.com>
2025-04-04 10:00:31 +0530

    Time: 21 ms (5.72%) | Memory: 45.9 MB (5.99%) - LeetSync

---
+++ 2406-decode-the-message/decode-the-message.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String decodeMessage(String key, String message) {
+           String uni = "";
+           String alpha = "abcdefghijklmnopqrstuvwxyz";
+           for(int i = 0;i<key.length();i++){
+               String temp = "";
+               temp += key.charAt(i);
+               if(!uni.contains(temp) && key.charAt(i) != ' ') uni += key.charAt(i);
+           }
+           String res = "";
+           for(int i = 0;i<message.length();i++){
+               if(message.charAt(i) != ' '){
+                 int in = uni.indexOf(message.charAt(i));
+                 res += alpha.charAt(in);
+               }
+               else{
+                  res += message.charAt(i);
+               }
+           }
+           return res;
+    }
+}
