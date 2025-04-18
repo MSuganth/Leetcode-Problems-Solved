8fa58c853d440a30881ba473c478e2949a397c9a
MSuganth <146002880+MSuganth@users.noreply.github.com>
2025-04-18 10:15:13 +0530

    Time: 30 ms (51.55%) | Memory: 64.4 MB (11.00%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+           int[] sarr = new int[arr.length];
+           for(int i = 0;i<arr.length;i++) sarr[i] = arr[i];
+           Arrays.sort(sarr);
+           Set<Integer> set = new LinkedHashSet<>();
+           Map<Integer,Integer> rank = new HashMap<>();
+           for(int i: sarr) set.add(i);
+           int r = 1;
+           for(int i:set){
+               rank.put(i,r);
+               r++;
+           }
+           for(int i = 0;i<arr.length;i++) arr[i] = rank.get(arr[i]);
+           //System.out.println(rank);
+           return arr;
+    }
+}
