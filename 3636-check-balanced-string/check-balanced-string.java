741b41ebe9af9c817192614a89aa4c6db98f739b
MSuganth <146002880+MSuganth@users.noreply.github.com>
2025-04-21 11:38:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (76.00%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isBalanced(String num) {
+           int e = 0;
+           int o = 0;
+           for(int i = 0;i<num.length();i++){
+              if(i%2 == 0) e += (num.charAt(i) - '0');
+              else o += (num.charAt(i)- '0');
+           }
+           if(e == o) return true;
+           return false;
+    }
+}
