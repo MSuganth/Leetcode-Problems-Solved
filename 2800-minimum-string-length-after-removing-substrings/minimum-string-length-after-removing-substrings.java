f2b95bf5cb36c794c03e3811415018e30c7ecde0
MSuganth <146002880+MSuganth@users.noreply.github.com>
2025-06-23 16:50:30 +0530

    Time: 11 ms (6.91%) | Memory: 44.3 MB (17.33%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.java
@@ -1,9 +1,20 @@
 class Solution {
     public int minLength(String s) {
-           while(s.contains("AB") || s.contains("CD")){
-                 if(s.contains("AB")) s = s.replace("AB","");
-                 else s = s.replace("CD","");
+           Stack<Character> stack = new Stack<>();
+           for(int i = 0;i<s.length();i++){
+                char ch = s.charAt(i);
+                if(!stack.isEmpty()){
+                     if(ch == 'B' && stack.peek() == 'A'){
+                            stack.pop();
+                     }
+                     else if(ch == 'D' && stack.peek() == 'C') {
+                            stack.pop();
+                     }
+                     else stack.push(ch);
+                }
+                else stack.push(ch);
            }
-           return s.length();
+           System.out.println(stack);
+           return stack.size();
     }
 }
