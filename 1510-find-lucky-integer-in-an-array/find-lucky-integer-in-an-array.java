998b0b5bc1e8f034c1234e65619a99ad8d018cbc
MSuganth <146002880+MSuganth@users.noreply.github.com>
2025-07-05 16:06:10 +0530

    Time: 5 ms (72.10%) | Memory: 43.4 MB (43.80%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
@@ -1,9 +1,16 @@
 class Solution {
     public int findLucky(int[] arr) {
            Map<Integer,Integer> map = new HashMap<>();
-           for(int i = 0;i<arr.length;i++) map.put(arr[i],map.getOrDefault(arr[i],0)+1);
-           int max = -1;
-           for(int i:map.keySet()) if(map.get(i) == i) if(i>max) max = i;
+           for(int i=0;i<arr.length;i++){
+               map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+           }
+           int max = 0;
+           for(int i:map.keySet()){
+              if(i == map.get(i)){
+                  if(i>max) max = i;
+              }
+           }
+           if(max == 0) return -1;
            return max;
     }
 }
