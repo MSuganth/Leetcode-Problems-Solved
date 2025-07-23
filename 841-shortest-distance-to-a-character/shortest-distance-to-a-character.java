2b15f9a93ba2f73c18abd2565e2277ff2339126d
MSuganth <146002880+MSuganth@users.noreply.github.com>
2025-07-23 22:46:55 +0530

    Time: 7 ms (23.88%) | Memory: 42.5 MB (68.36%) - LeetSync

---
+++ 841-shortest-distance-to-a-character/shortest-distance-to-a-character.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] shortestToChar(String s, char c) {
+           int[] arr = new int[s.length()];
+           List<Integer> list = new ArrayList<>();
+           for(int i=0;i<s.length();i++){
+              if(s.charAt(i) == c) list.add(i);
+           }
+           for(int i =0;i<s.length();i++){
+               int min = Integer.MAX_VALUE;
+               for(int j=0;j<list.size();j++){
+                   min = Math.min(min,Math.abs(i-list.get(j)));
+               }
+               arr[i] = min;
+           }
+           return arr;
+    }
+}
