53401f8391a28b05a83806df7f99fa338756227d
MSuganth <146002880+MSuganth@users.noreply.github.com>
2025-07-28 22:38:55 +0530

    Time: 9 ms (76.27%) | Memory: 44.6 MB (5.02%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+           Set <Character> set1 = new LinkedHashSet<>();
+           Set <Character> set2 = new LinkedHashSet<>();
+           Map<Character,Character> map = new LinkedHashMap<>();
+           for(char ch:s.toCharArray()) set1.add(ch);
+           for(char ch:t.toCharArray()) set2.add(ch);
+           if(set1.size() == set2.size()){
+              List<Character> l1 = new ArrayList<>();
+              List<Character> l2 = new ArrayList<>();
+              for(char ch:set1) l1.add(ch);
+              for(char ch:set2) l2.add(ch);
+              for(int i = 0;i<l1.size();i++) map.put(l1.get(i),l2.get(i));
+              StringBuilder res = new StringBuilder();
+              for(char ch:s.toCharArray()){
+                 res.append(map.get(ch));
+              }
+              if(t.equals(res.toString())) return true;
+              return false;
+           };
+           return false;
+    }
+}
