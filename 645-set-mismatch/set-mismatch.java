2ac868696299bc804092b4274f86551aa9c6619e
MSuganth <146002880+MSuganth@users.noreply.github.com>
2025-09-10 10:34:47 +0530

    Time: 12 ms (28.87%) | Memory: 45.5 MB (71.89%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -2,17 +2,17 @@ class Solution {
     public int[] findErrorNums(int[] nums) {
         Set<Integer> set = new HashSet<>();
         int n = nums.length;
-        int o = 0;
+        int[] res = new int[2];
         int sum = 0;
         for(int i:nums){
-              if(set.contains(i)) o = i;
+              if(set.contains(i)) res[0] = i;
               else{
                 sum += i;
                 set.add(i);
               }
         }
         int total = (n*(n+1))/2;
-        int s = total - sum;
-        return new int[]{o,s};
+        res[1] = total - sum;
+        return res;
     }
 }
