47f218e15a4fde643a887b781d8f38091190bf5b
MSuganth <146002880+MSuganth@users.noreply.github.com>
2025-09-18 19:05:18 +0530

    Time: 5 ms (98.83%) | Memory: 44.2 MB (89.88%) - LeetSync

--- 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.java
+++ 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.java
@@ -1,13 +1,28 @@
 class Solution {
     public int countWords(String[] w1, String[] w2) {
-           List<String> a1 = Arrays.asList(w1);
-           List<String> a2 = Arrays.asList(w2);
-           int count = 0;
-           for(String i : a1){
-              int f1 = Collections.frequency(a1,i);
-              int f2 = Collections.frequency(a2,i);
-              if(f1 == 1 && f2 == 1) if(f1 == f2) count++; 
+           Set<String> set1 = new HashSet<>();
+           Set<String> set2 = new HashSet<>();
+           Set<String> rem = new HashSet<>();
+           int res = 0;
+           for(String s:w1){
+               if(set1.contains(s)){
+                  rem.add(s);
+                  set1.remove(s);
+               }
+               else set1.add(s);
            }
-           return count;
+           for(String s:w2){
+               if(set2.contains(s)){
+                  rem.add(s);
+                  set2.remove(s);
+               }
+               else{
+                   if(!(rem.contains(s))) set2.add(s);
+               }
+           }
+           for(String s:set1){
+               if(set2.contains(s)) res++;
+           }
+           return res;
     }
 }
