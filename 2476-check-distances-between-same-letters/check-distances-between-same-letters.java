ad92d72533e1ae54e6f56b5efad58b3c62ff0b39
MSuganth <146002880+MSuganth@users.noreply.github.com>
2025-09-21 12:49:54 +0530

    Time: 1 ms (86.74%) | Memory: 42.6 MB (91.30%) - LeetSync

--- 2476-check-distances-between-same-letters/check-distances-between-same-letters.java
+++ 2476-check-distances-between-same-letters/check-distances-between-same-letters.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean checkDistances(String s, int[] d) {
-           boolean[] b = new boolean[s.length()];
+           boolean[] b = new boolean[26];
            for(int i = 0;i<s.length();i++){
-               if(b[i]) continue;
+               int t1 = (int) s.charAt(i);
+               if(b[t1-97]) continue;
                int j = i+1;
                while(j<s.length()){
                    if(s.charAt(i) == s.charAt(j)){
-                      b[j] = true;
                       break;
                    }
                    j++;
                }
-               int t1 = (int) s.charAt(i);
+               b[t1-97] = true;
                if(!(d[t1-97] == (j-i-1))) return false;
            }
            return true;
