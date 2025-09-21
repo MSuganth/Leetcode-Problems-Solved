9a0d830d309c13d7cda48cf725ec5fc11e5767ab
MSuganth <146002880+MSuganth@users.noreply.github.com>
2025-09-21 12:24:25 +0530

    Time: 2 ms (74.68%) | Memory: 43 MB (8.00%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String makeGood(String s) {
+           boolean c = true;
+           while(c){
+                StringBuilder t = new StringBuilder(s);
+                boolean f = false;
+                for(int i = 0;i<t.length()-1;i++){
+                     int t1 = (int) t.charAt(i);
+                     int t2 = (int) t.charAt(i+1);
+                     if( (t1-32) == t2){
+                          f = true;
+                          t.deleteCharAt(i);
+                          t.deleteCharAt(i);
+                          break;
+                     }
+                     else if( (t1+32) == t2){
+                          f = true;
+                          t.deleteCharAt(i);
+                          t.deleteCharAt(i);
+                          break;
+                     }
+                }
+                // System.out.println(t);
+                s = t.toString();
+                if(!f) c = false;
+           }
+           return s;
+    }
+}
